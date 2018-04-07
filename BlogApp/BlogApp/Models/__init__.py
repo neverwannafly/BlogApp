b8caeadf0b56b7d6ebf453ddff85b7ee3a7c2a97
Shubham Anand <shubham_chess@live.com>
2018-04-07 13:40:50 +0530

    Update models to be used as modules

---
+++ BlogApp/BlogApp/Models/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["Post", "Location_Manager", "Video", "init_database"]
+
+from Models import Post
+from Models import Location_Manager
+from Models import Video
+from InitDB import init_database
